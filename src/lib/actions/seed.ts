67154717670a949dd28ceff232bcfa576c0d479d
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2025-01-19 17:28:50 +0330

    seeding cities to db

---
+++ src/lib/actions/seed.ts
@@ -0,0 +1,35 @@
+'use server'
+import provincesData from '../../constants/cities.json' // Path to your JSON data
+import { prisma } from '../prisma'
+
+export async function seed() {
+  try {
+    for (const provinceData of provincesData) {
+      const { name, center, latitude, longitude, cities } = provinceData
+
+      const province = await prisma.province.create({
+        data: {
+          name,
+          center,
+          latitude: parseFloat(latitude),
+          longitude: parseFloat(longitude),
+          cities: {
+            create: cities?.map((city) => ({
+              name: city.name,
+              latitude: parseFloat(city.latitude),
+              longitude: parseFloat(city.longitude),
+            })),
+          },
+        },
+      })
+
+      console.log(
+        `Created province: ${province?.name} with ${cities?.length} cities`
+      )
+    }
+    console.log('Seeding completed successfully')
+  } catch {
+    console.log('Error seeding the database:')
+  }
+}
+seed()
