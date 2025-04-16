4c206b96acf16d08c43f4d9dcde6fd7a233cae5e
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2025-04-16 14:03:13 +0330

    cart page

---
+++ src/app/[locale]/(store)/goshop/cart/page.tsx
@@ -0,0 +1,28 @@
+import { cookies } from 'next/headers'
+import Header, { Country } from '../components/header/header'
+import CartContainer from '../components/cart/container'
+
+export default function CartPage() {
+  // Get cookies from the store
+  const cookieStore = cookies()
+  const userCountryCookie = cookieStore.get('userCountry')
+
+  // Set default country if cookie is missing
+  let userCountry: Country = {
+    name: 'United States',
+    city: '',
+    code: 'US',
+    region: '',
+  }
+
+  // If cookie exists, update the user country
+  if (userCountryCookie) {
+    userCountry = JSON.parse(userCountryCookie.value) as Country
+  }
+  return (
+    <>
+      <Header />
+      <CartContainer userCountry={userCountry} />
+    </>
+  )
+}
---
+++ src/app/[locale]/(store)/goshop/components/cart/container.tsx
@@ -0,0 +1,113 @@
+'use client'
+import { useCartStore } from '@/cart-store/useCartStore'
+import useFromStore from '@/hooks/useFromStore'
+
+import React, { useEffect, useState, useRef } from 'react'
+import CartHeader from './car-header'
+import CartSummary from './summary'
+import EmptyCart from './empty-cat'
+import { Country } from '../header/header'
+import { CartProductType } from '../../types'
+import { SecurityPrivacyCard } from '../shipping/returns-security-privacy-card'
+import CartProduct from './cart-product'
+// import CartProduct from "../cards/cart-product";
+// import FastDelivery from "../cards/fast-delivery";
+// import { SecurityPrivacyCard } from "../product-page/returns-security-privacy-card";
+// import { updateCartWithLatest } from "@/queries/user";
+// import CountryNote from "../shared/country-note";
+
+export default function CartContainer({
+  userCountry,
+}: {
+  userCountry: Country
+}) {
+  const cartItems = useFromStore(useCartStore, (state) => state.cart) || []
+  const setCart = useCartStore((state) => state.setCart)
+
+  const [loading, setLoading] = useState<boolean>(false)
+  const [isCartLoaded, setIsCartLoaded] = useState<boolean>(false)
+  const [selectedItems, setSelectedItems] = useState<CartProductType[]>([])
+  const [totalShipping, setTotalShipping] = useState<number>(0)
+
+  // Ref to track if the component has mounted
+  const hasMounted = useRef(false)
+
+  useEffect(() => {
+    if (cartItems !== undefined) {
+      setIsCartLoaded(true) // Flag indicating cartItems has finished loading
+    }
+  }, [cartItems])
+
+  useEffect(() => {
+    const loadAndSyncCart = async () => {
+      try {
+        setLoading(true)
+        // const updatedCart = await updateCartWithLatest(cartItems);
+        // setCart(updatedCart);
+        setLoading(false)
+      } catch (error) {
+        setLoading(false)
+      }
+    }
+
+    // Run only when userCountry changes and after the initial mount
+    if (hasMounted.current && cartItems?.length) {
+      loadAndSyncCart()
+    } else {
+      hasMounted.current = true // Set the ref to true after the first render
+    }
+  }, [userCountry])
+
+  return (
+    <div>
+      {cartItems && cartItems.length > 0 ? (
+        <>
+          <div className="bg-[#f5f5f5] min-h-[calc(100vh-65px)] px-2">
+            <div className="max-w-[1200px] mx-auto py-4 flex flex-col gap-y-4 lg:flex-row">
+              <div className="min-w-0 flex-1">
+                {/* Cart header */}
+                <CartHeader
+                  cartItems={cartItems}
+                  selectedItems={selectedItems}
+                  setSelectedItems={setSelectedItems}
+                />
+                {/* <div className="my-2">
+                  <CountryNote country={userCountry.name} />
+                </div> */}
+                <div className="h-auto overflow-x-hidden overflow-auto mt-2">
+                  {/* Cart items */}
+                  {cartItems.map((product) => (
+                    <CartProduct
+                      key={`${product.productSlug}-${product.variantSlug}`}
+                      product={product}
+                      selectedItems={selectedItems}
+                      setSelectedItems={setSelectedItems}
+                      setTotalShipping={setTotalShipping}
+                      userCountry={userCountry}
+                    />
+                  ))}
+                </div>
+              </div>
+              {/* Cart side */}
+              <div className="sticky top-4 lg:ml-5 w-full lg:w-[380px] max-h-max">
+                {/* Cart summary */}
+                <CartSummary
+                  cartItems={cartItems}
+                  shippingFees={totalShipping}
+                />
+                {/* <div className="mt-2 p-4 bg-white px-6">
+                  <FastDelivery />
+                </div> */}
+                <div className="mt-2 p-4 bg-white px-6">
+                  <SecurityPrivacyCard />
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <EmptyCart />
+      )}
+    </div>
+  )
+}
